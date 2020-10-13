359484a4e2c81b4b1ba1465595ddb2bff69f59f1
saikrishnareddt0306 <62168396+saikrishnareddt0306@users.noreply.github.com>
2020-10-13 04:38:13 -0400

    Add files via upload

---
+++ src/main/java/com/example/BootspringApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootspringApplication.class, args);
+	}
+
+}
